b090aeec986f844b131eb7f73ff62b3e943d50e8
Jaksell69 <jaksellzarza26@gmail.com>
2024-07-23 22:44:36 -0600

    Delete Tuzistemas-app directory

--- Tuzistemas-app/vendor/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Eliminar la cookie estableciendo una fecha de expiración en el pasado
-setcookie("user", "", time() - 3600, "/");
-
-// Redirigir al formulario de login
-header("Location: ../../index.php");
-exit();
-?>
